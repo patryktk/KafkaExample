30eb14a9ff62c1a7784cf7f3e5ca87a280d081d9
patryktk <patrick.tkaczyk@gmail.com>
2026-01-16 13:07:39 +0100

    Initial commit

---
+++ ProductsMicroservice/src/main/java/pl/tkaczyk/productsmicroservice/ProductsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.tkaczyk.productsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsMicroserviceApplication.class, args);
+    }
+
+}
